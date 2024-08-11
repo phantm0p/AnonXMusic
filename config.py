ec776a1f296f9442da7f72dc907417fca393ea46
phantm0p <111131477+phantm0p@users.noreply.github.com>
2024-08-11 15:36:00 +0530

    Update config.py
    Signed-off-by: phantm0p <111131477+phantm0p@users.noreply.github.com>

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = "mongodb+srv://darkth0ughtss00:loniko0908@music.njvuzcz.mongodb.n
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = -1002069412308
+LOGGER_ID = -1002000681699
 
 
 # Get this value from @FallenxBot on Telegram by /id
